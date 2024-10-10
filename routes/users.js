795bdc75fd1721f65a8d057f00499f3623abcb6f
gauthier <alicia.gauthier@student.vinci.be>
2024-10-10 16:13:50 +0200

    gestion si l'user est actif

--- routes/users.js
+++ routes/users.js
@@ -28,21 +28,7 @@ router.post('/login', (req, res, next) => {
             res.redirect('/users');
         }
         else {
-            if (bcrypt.compareSync(req.body.userPassword, userFound.password)) {
-                console.log("password correct");
-                req.session.login = req.body.userLogin;
-                req.session.connected = true;
-                if (userFound.admin) {
-                    req.session.admin = true;
-                    res.redirect('/admin');
-                } else {
-                    req.session.admin = false;
-                    res.redirect('/members');
-                }
-            }
-            else {
-                passwordManagement(req, res);
-            }
+            ifUserActiveManagement(req, userFound, res);
         }
     }
     else {
@@ -94,6 +80,24 @@ router.post('/add', (req, res, next) => {
 });
 
 module.exports = router;
+
+function ifUserActiveManagement(req, userFound, res) {
+    if (bcrypt.compareSync(req.body.userPassword, userFound.password)) {
+        console.log("password correct");
+        req.session.login = req.body.userLogin;
+        req.session.connected = true;
+        if (userFound.admin) {
+            req.session.admin = true;
+            res.redirect('/admin');
+        } else {
+            req.session.admin = false;
+            res.redirect('/members');
+        }
+    }
+    else {
+        passwordManagement(req, res);
+    }
+}
 
 function passwordManagement(req, res) {
     console.log("bad password");
